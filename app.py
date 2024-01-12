78b7d388ebcef1e265e1acc22b9e76b90028537b
Martin <martin.evenbom@gmail.com>
2024-01-12 16:19:53 +0100

    updated with dark-mode

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, session
 
 app = Flask(__name__)
+
+def read_input(input):
+    with open(input, 'r') as f:
+        return f.readline().rstrip()
+
+secret_key = read_input('secret_key.txt')
+app.secret_key = secret_key
 
 projects = [
     {
         "name": "NES emulator in rust-lang",
-        "thumb": "img/habit-tracking.png",
-        "hero": "img/habit-tracking-hero.png",
+        "thumb": "img/nes.png",
+        "hero": "img/nes.png",
         "categories": ["rust", "low-level"],
         "slug": "rust",
         "prod": "https://nes.evenbom.se/"
     },
 ]
 slug_to_project = {project["slug"]: project for project in projects}
+
+@app.route('/toggle_dark_mode', methods=['POST'])
+def toggle_dark_mode():
+    session['dark_mode'] = not session.get('dark_mode', False)
+    return '', 204
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -30,8 +42,6 @@ def about():
 def contact():
     return render_template("contact.html")
 
-
-
 @app.route("/project/<string:slug>")
 def project(slug):
     if slug not in slug_to_project:
@@ -39,4 +49,4 @@ def project(slug):
     return render_template(f"project_{slug}.html", project=slug_to_project[slug])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)

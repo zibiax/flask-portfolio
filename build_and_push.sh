6659ef17feb8385012620d2509500c9aca181641
Martin <martin.evenbom@gmail.com>
2024-02-05 16:23:28 +0100

    fixed build_and_push script

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
+source docker_credentials.txt
+
+IMAGE_NAME="flask_portfolio"
+TAG="latest"
+CONTAINER_NAME="flask_portfolio"
+
+# Docker login
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+
+# Stop and remove the existing container
+docker stop $CONTAINER_NAME
+docker rm $CONTAINER_NAME
+
 # Build Docker image
-docker build -t zibax/flask_portfolio:latest .
+docker build -t zibax/$IMAGE_NAME:$TAG .
+
+# Tag the image
+docker tag $IMAGE_NAME:$TAG zibax/$IMAGE_NAME:$TAG
 
 # Push to Docker.io
-sudo docker push zibiax.azurecr.io/blog_1:latest
+sudo docker push zibax/flask_portfolio:latest
+
+# Run the new container
+docker run -d --name $CONTAINER_NAME -p 5000:5000 zibax/$IMAGE_NAME:$TAG
 
 # Save image to RPI
-docker save zibax/flask_portfolio:latest | ssh martin@rpi-ip 'docker load'
+#docker save zibax/flask_portfolio:latest | ssh martin@rpi-ip 'docker load'

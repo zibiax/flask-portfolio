4e878ce798c24d066b71a56ef97edc52b566f245
Martin <martin.evenbom@gmail.com>
2024-04-29 21:40:53 +0200

    default password update

--- models.py
+++ models.py
@@ -1,14 +1,30 @@
 from flask_login import UserMixin
-from flask_sqlalchemy import SQLAlchemy
 from app import db
+import os
+
+secret_password = os.getenv("SECRET_PASSWORD")
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+    @classmethod
+    def create_default_user(cls):
+        default_user = cls(username='martin', password=secret_password)
+        db.session.add(default_user)
+        db.session.commit()
+
+
     def __repr__(self) -> str:
         return f'<User {self.username}>'
+
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,6 +32,7 @@ class Project(db.Model):
     description = db.Column(db.String(255), nullable=False)
     production_url = db.Column(db.String(255))
     image = db.Column(db.String(255))
+
 
     def __repr__(self):
         return f'<Project {self.name}>'
